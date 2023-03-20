f886c2eb92f79024510809d40db92ec14e4c9582
Sage Schoene <jschoe14@Jonathans-MacBook-Pro.local>
2023-03-20 19:12:39 -0400

    progress

---
+++ src/components/Header.vue
@@ -0,0 +1,107 @@
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      showDropdown: false,
+      activeIndex: "0",
+      activeLink: null,
+    };
+  },
+  mounted() {},
+
+  methods: {
+    toggleHamburger() {
+      this.showDropdown = !this.showDropdown;
+    },
+    handleSelect(val) {
+      this.activeIndex = val;
+      this.$router.push({path: val})
+        this.showDropdown = false;
+    },
+  },
+};
+</script>
+
+<template>
+    
+  <div class="nav">
+    
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      :ellipsis="false"
+      @select="handleSelect"
+    >
+      <el-menu-item :router="true" :default-active="activeLink" index="/"
+        ><img
+          src="src/assets/route40shield.jpeg"
+          alt="Route 40 Paintball"
+          height="60"
+      /></el-menu-item>
+      <div class="flex-grow" />
+      <template v-for="rule in $router.options.routes" :key="rule.path">
+        <el-menu-item v-if="rule.name !== 'Home'" :index="rule.path">{{ rule.name }}</el-menu-item>
+      </template>
+    </el-menu>
+  </div>
+  <div class="mobilenav">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      :ellipsis="false"
+      @select="handleSelect"
+    >
+      <el-menu-item index="/"
+        ><img
+          src="src/assets/route40shield.jpeg"
+          alt="Route 40 Paintball"
+          height="60"
+      /></el-menu-item>
+      <div class="flex-grow" />
+      <div style="padding-right:10px;" @click="toggleHamburger">
+        <i class="center fa fa-bars" style="font-size: 40px"></i>
+      </div>
+    </el-menu>
+    <div v-if="this.showDropdown" class="dropdown">
+      <template v-for="rule git push -u origin mainin $router.options.routes" :key="rule.path">
+      <div v-if="rule.name !== 'Home'" class="dropdown-item" @click="handleSelect(rule.path)">{{rule.name}}</div>
+      </template>
+    </div>
+  </div>
+  <!-- <img    
+          src="src/assets/40_header.jpeg"
+          alt="Route 40 Paintball"
+      /> -->
+</template>
+
+<style scoped>
+.dropdown-item {
+  margin: auto;
+  width: 100%;
+  padding: 10px;
+  text-align: center;
+  letter-spacing: 0.6px;
+  background-color: rgba(193, 200, 207, 0.64);
+  border-bottom: 1px solid rgba(193, 200, 207);
+}
+.center {
+  margin-top: 10px;
+}
+.flex-grow {
+  flex-grow: 1;
+}
+.mobilenav {
+  display: none;
+}
+@media only screen and (max-width: 768px) {
+  .nav {
+    display: none;
+  }
+  .mobilenav {
+    display: block;
+  }
+}
+</style>
---
+++ src/views/Events.vue
@@ -0,0 +1,47 @@
+<script setup>
+</script>
+
+<template>
+THIS IS THE EVENTS PAGE
+  
+</template>
+
+<style scoped>
+@media only screen and (max-width: 768px) {
+  .carousel {
+    display: none;
+  }
+  .mobile-carousel{
+    display: block !important;
+    margin-top: 20px;;
+  }
+}
+.carousel {
+  margin-top: 20px;
+}
+.mobile-carousel {
+  display: none;
+}
+.header {
+  margin: auto;
+  width: 75%;
+  padding: 10px;
+  text-align: center;
+  letter-spacing: 0.6px;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
